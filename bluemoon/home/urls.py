85b462ec44a7b098249cb385ce2cc440ccf1b22d
Kaushal1011 <kaushalpatil10@gmail.com>
2019-10-31 04:10:07 +0530

    Made Base template and Home Page

--- bluemoon/home/urls.py
+++ bluemoon/home/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('',views.index, name='Programming'),
+    path('',views.index, name='Music'),
+    path('',views.index, name='Design'),
+
 ]

e32dc5c7fd03c3d2d2d5704302093eab115d6d20
Kaushal1011 <kaushalpatil10@gmail.com>
2019-10-31 16:16:14 +0530

    Added Music App and urlroutes

--- bluemoon/bluemoon/urls.py
+++ bluemoon/bluemoon/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('home.urls')),
+    path('music/',include('music.urls')),
     path('admin/', admin.site.urls),
 ]
--- bluemoon/music/urls.py
+++ bluemoon/music/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
+from home import views as home_view
 from . import views
-from music import views as music_views
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', home_view.index, name='index'),
     path('',views.index, name='Programming'),
-    path('music/',music_views.index, name='Music'),
+    path('',views.index, name='Music'),
     path('',views.index, name='Design'),
 
 ]

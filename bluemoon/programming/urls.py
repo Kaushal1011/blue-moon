17080900dd5a3c6330b4b7a6d79ff9d6f0649717
Kaushal1011 <kaushalpatil10@gmail.com>
2020-02-04 12:12:20 +0530

    Configured urls for programming markdown details

--- bluemoon/programming/urls.py
+++ bluemoon/programming/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from home import views as home_view
 from music import views as music_view
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', home_view.index, name='index'),
     path('', views.index, name='Programming'),
     path('../music/', music_view.index, name='Music'),
+    re_path(r'(?P<title>[-a-zA-Z0-9_]+)/$',
+            views.readmore, name="detail"),
     path('', views.index, name='Design'),
 
 ]
--- bluemoon/programming/views.py
+++ bluemoon/programming/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from programming.models import Post
 # Create your views here.
@@ -7,3 +8,11 @@ def index(request):
     postlist = Post.objects.all()
     context = {'post': postlist}
     return render(request, 'programming/index.htm', context)
+
+
+def readmore(request, title):
+    try:
+        p = Post.objects.get(title=title)
+    except Post.DoesNotExist:
+        raise Http404("Post does not exist")
+    return render(request, 'programming/detail.htm', {'Post': p})
